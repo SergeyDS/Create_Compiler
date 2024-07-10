6c3e28646a93c3c3c4e1b39b8200128b57b24942
Sergey <sergey_1c@mail.ru>
2024-07-10 21:21:39 +0700

    Первоначальная заготовка Понимаю код гавно.но надо что бы что  то заработало

--- Create_Compiler/Create_Compiler.cpp
+++ Create_Compiler/Create_Compiler.cpp
@@ -1,20 +1,131 @@
-﻿// Create_Compiler.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include <fstream>
 #include <iostream>
+#include <optional>
+#include <sstream>
+#include <vector>
+using namespace std;
 
-int main()
+
+enum class TokenType
 {
-    std::cout << "Hello World!\n";
+	_return,
+	int_lit,
+	semi
+};
+
+struct Token
+{
+	TokenType type;
+	optional<string> value {};
+};
+
+vector<Token> tokenize(const string &str){
+	vector<Token> tokens {};
+	string buf;
+	for (int i = 0; i < str.length();i++)
+	{
+		char c = str.at(i);
+		if (isalpha(c))
+		{
+			buf.push_back(c);
+			i++;
+			while (isalnum(str.at(i)))
+			{
+				buf.push_back(str.at(i));
+				i++;
+			}
+			i--;
+
+			if (buf == "return")
+			{
+				tokens.push_back({.type = TokenType::_return});
+				buf.clear();
+				continue;
+			}
+			else
+			{
+				cerr << "You messed up!" << endl;
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if(isdigit(c)){
+			buf.push_back(c);
+			i++;
+			while (isdigit(str.at(i)))
+			{
+				buf.push_back(str.at(i));
+				i++;
+			}
+			i--;
+			tokens.push_back({.type = TokenType::int_lit,.value = buf});
+			buf.clear();
+		}
+		else if (c == ';')
+		{
+			tokens.push_back({.type = TokenType::semi});
+		}
+		else if (isspace(c))
+		{
+			continue;
+		}
+		else
+		{
+			cerr << "You messed up!" << endl;
+			exit(EXIT_FAILURE);
+		}
+	}
+	return tokens;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+string tokens_to_asm(const vector<Token> &tokens){
+	stringstream output;
+	output << "global _start\n_start:\n";
+	
+	for (int i = 0; i < tokens.size(); i++)
+	{
+		const Token &token = tokens.at(i);
+		if (token.type == TokenType::_return)
+		{
+			if (i+1 < tokens.size() && tokens.at(i+1).type == TokenType::int_lit)
+			{
+				if (i + 2 < tokens.size() && tokens.at(i+2).type == TokenType::semi)
+				{
+					output << "    mov rax,60\n";
+					output << "    mov rdi, " << tokens.at(i+1).value.value() << "\n";
+					output << "    syscall";
+				}
+			}
+		}
+	}
+	return output.str();
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cerr << "Incorrect usage. Correct usage is..." << endl;
+		cerr << "hydro <input.hy>" << endl;
+		return EXIT_FAILURE;
+	}
+   
+	string contents;
+	{
+		stringstream contents_stream;
+		fstream input(argv[1], ios::in);
+		contents_stream << input.rdbuf();
+		contents = contents_stream.str();
+	}
+
+	vector<Token> tokens = tokenize(contents);
+	{
+		fstream file("out.asm", ios::out);
+		
+		file << tokens_to_asm(tokens);
+	}
+	
+
+	cout << "!";
+   return EXIT_SUCCESS;
+}
+
